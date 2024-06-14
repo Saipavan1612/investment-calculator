662e1d92d15721a5c864b9030b8793b33c8cd17e
Saipavan1612 <saipavangurram@gmail.com>
2024-06-14 13:52:10 +0530

    Investment calculator

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number
+  }>();
+
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '10';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.initialInvestment,
+      annualInvestment: +this.annualInvestment,
+      expectedReturn: +this.expectedReturn,
+      duration: +this.duration,
+    });
+  }
+}
